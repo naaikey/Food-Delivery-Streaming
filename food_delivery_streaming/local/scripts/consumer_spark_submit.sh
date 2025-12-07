b885bfeaa37880576b9fe264f19d1f9832edb6d1
Nikhilesh Kumar <nikhileshkumar97@gmail.com>
2025-12-07 21:41:14 +0530

    Delete food_delivery_streaming/local directory

--- food_delivery_streaming/local/scripts/consumer_spark_submit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.postgresql:postgresql:42.7.3 consumers/orders_stream_consumer.py --config configs/orders_stream.yml
